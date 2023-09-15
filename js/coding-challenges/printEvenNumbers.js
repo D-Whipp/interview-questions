c566e2181710cb52dc57e9fe378e02df50170e2d
David Whipple <dwhipp88@gmail.com>
2023-09-14 18:52:26 -0600

    added coding challenge, went over html questions

---
+++ js/coding-challenges/printEvenNumbers.js
@@ -0,0 +1,16 @@
+// https://www.codecademy.com/resources/blog/10-javascript-code-challenges-for-beginners/
+
+// Print all even numbers from 0 to 10
+// Try to make the solution to this problem as efficiently as possible. Consider using loops that would allow you to type the fewest characters/commands. While you could simply print the even numbers, get creative and see how you could output them in a way that would work up to 10 or even up to 10,000 with little extra effort
+// let i = 0;
+// console.log('hi');
+// while (i <= 10) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+for (let i = 0; i < 12; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
